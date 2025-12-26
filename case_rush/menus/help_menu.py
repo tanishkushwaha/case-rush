76361d48eea7fd49db366dc67732be4500e75241
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2025-12-26 13:24:45 +0530

    Add data dir path reveal in the help menu

--- case_rush/menus/help_menu.py
+++ case_rush/menus/help_menu.py
@@ -4,10 +4,11 @@ Provides help info for the app.
 
 from rich import print as rprint
 
-from case_rush.paths import CONFIG_DIR, ITEMS_DIR
+from case_rush.paths import CONFIG_DIR, ITEMS_DIR, DATA_DIR
 
 
 def help_menu():
     rprint("\n[bold]----- Help -----[/bold]\n")
     rprint(f"Items Directory: {ITEMS_DIR}")
+    rprint(f"Data Directory: {DATA_DIR}")
     rprint(f"Config File: {CONFIG_DIR / 'config.toml'}")
